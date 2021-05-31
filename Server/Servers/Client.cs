a2ce29e2b1589a6f52b950e117a6951305cbe143
sahncagri <46481922+sahncagri@users.noreply.github.com>
2021-05-31 18:05:51 +0300

    first Commit

---
+++ Server/Servers/Client.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Servers
+{
+    class Client
+    {
+        private Socket clientSocket;
+        private Server server;
+        private IPEndPoint endPoint;
+
+        public Client(Socket _clientSocket,IPEndPoint endPoint, Server server)
+        {
+            this.clientSocket = _clientSocket;
+            this.endPoint = endPoint;
+            this.server = server;
+        }
+
+        public void Start()
+        {
+            clientSocket.Connect(endPoint);
+            StartReceive();
+        }
+        private void StartReceive()
+        {
+            clientSocket.BeginReceive();
+        }
+
+        
+    }
+}
---
+++ Server/Servers/Message.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+
+namespace Server.Servers
+{
+    class Message
+    {
+        private byte[] data = new byte[1024];
+        private int startIndex = 0;
+
+        public byte[] Data
+        {
+            get { return data; }
+        }
+        public int StartIndex
+        {
+            get { return startIndex; }
+        }
+        public int RemainSize
+        {
+            get { return data.Length - startIndex; }
+        }
+
+
+        public void ReadMessage(int newDataAmount,Action<ActionCode,RequestCode,string> OnReadMessage)
+        {
+            
+        }
+    }
+}
